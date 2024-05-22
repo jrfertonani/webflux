405d4ec111da00fa2fc1620c4e45e06f9450f3e6
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-05-22 10:09:10 -0300

    add unit test to delete user service

--- src/test/java/springwebflux/service/UserServiceTest.java
+++ src/test/java/springwebflux/service/UserServiceTest.java
@@ -99,10 +99,20 @@ class UserServiceTest {
                 .verify();
 
         verify(repository, times(1)).save(any(User.class));
-
     }
 
     @Test
     void delete() {
+        User entity = User.builder().build();
+        when(repository.findAndRemove(anyString())).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.delete("123");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findAndRemove(anyString());
     }
 }
